4394bc11241097bd6981fbe705c8420bfbd66244
Shriram Saravanan <shriram.saravanan@solitontech.local>
2025-01-08 11:05:50 +0530

    ADD : BankAccount Class

---
+++ OOPs/Hierarchy_Tasks/BankingSystem.cs
@@ -0,0 +1,33 @@
+﻿namespace OOPs.Hierarchy_Tasks;
+
+public abstract class BankAccount
+{
+    private string _Account_Number {  get; set; }
+    private decimal _balance { get; set; }
+
+
+    public BankAccount(string accountNumber, decimal balance)
+    {
+        _Account_Number = accountNumber;
+        _balance = balance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        _balance = _balance + amount;
+        Console.WriteLine($"\n {amount} Deposited.\nCurrent Balance = {_balance} ");
+    }
+
+    public void WithDraw(decimal amount) {
+        if (amount > _balance) 
+        {
+            Console.WriteLine("Insufficient Funds in Account");
+        }
+        else 
+        {
+            _balance = _balance - amount;
+            Console.WriteLine($"\n{amount} WithDrawn.\nCurrent Balance = {_balance}");
+        }
+     }
+
+}
