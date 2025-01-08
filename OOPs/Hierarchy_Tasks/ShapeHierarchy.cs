368f76373f64e770362b7ce33c16e764043eae0c
Shriram Saravanan <shriram.saravanan@solitontech.local>
2025-01-08 09:44:40 +0530

    ADD : Shape Hierarchy feature

--- OOPs/Hierarchy_Tasks/ShapeHierarchy.cs
+++ OOPs/Hierarchy_Tasks/ShapeHierarchy.cs
@@ -1,1 +1,67 @@
-﻿
+﻿namespace OOPs.ShapeHierarchy;
+
+
+
+public abstract class Shape
+{
+    public abstract string Colour { get; set; }
+    public abstract double CalculateArea();
+    public void PrintDetails()
+    {
+        Console.WriteLine($"Colour : {Colour}, Area :{CalculateArea()}");
+
+    }
+}
+
+public class Rectangle : Shape
+{
+    public override string Colour { get; set; }
+    private readonly string _shapeType = "Rectangle";
+    public int Length { get; set; }
+    public int Width { get; set; }
+
+    public Rectangle(string colour, int length, int width)
+    {
+        Colour = colour;
+        Length = length;
+        Width = width;
+
+    }
+
+    public override double CalculateArea()
+    {
+        return this.Length * this.Width;
+    }
+    public new void PrintDetails()
+    {
+        Console.WriteLine($"Colour : {Colour}, ShapeType : {_shapeType} ,Area :{CalculateArea()}");
+    }
+}
+
+public class Circle : Shape
+{
+
+    public override string Colour { get; set; }
+    private readonly string _shapeType = "Circle";
+
+    private double _radius;
+
+    private const double Pi = Math.PI;
+
+    public Circle(string colour, double radius)
+    {
+        Colour = colour;
+        _radius = radius;
+    }
+    public override double CalculateArea()
+    {
+        return 2 * Pi * _radius;
+    }
+
+    public new void PrintDetails()
+    {
+        Console.WriteLine($"Colour : {Colour}, ShapeType : {_shapeType} ,Area :{CalculateArea()}");
+
+    }
+
+}
--- OOPs/Program.cs
+++ OOPs/Program.cs
@@ -1,4 +1,13 @@
-﻿
+﻿using OOPs.ShapeHierarchy;
+
+Rectangle rectangle = new Rectangle("Blue", 4, 7);
+Circle circle = new Circle("Yellow", 5.3);
+rectangle.PrintDetails();
+circle.PrintDetails();
 
 
-Console.WriteLine("OOPS Assignment 2");
+class Employee
+{
+    public string Name { get; set; }
+    public decimal Salary { get; set; }
+}
